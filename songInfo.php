482451682ea4bc43c4b291a118bf789636c22af9
BlastFM Limited <30941076+BlastFM@users.noreply.github.com>
2019-11-07 18:39:48 +0000

    Create songInfo.php
    If you only want to get the information for the current playing artist/track, this is about the quickest way to do it.

---
+++ songInfo.php
@@ -0,0 +1,17 @@
+<?php
+//	Shoutcast2
+	$server = '45.33.5.23';
+	$port = '8000';
+	$streamUrl = "http://{$server}:{$port}/currentsong?sid=1";
+	$streamData = @file_get_contents($streamUrl);
+if ($streamData === FALSE) {
+	echo "Unable to read stream data from ".$streamUrl;
+	exit();
+}
+//	The regular expression that splits the song info into Artist/Title. Usually escaped space hyphon space.
+	$splitSongRegex = "/\ \-\ /";
+	$songArray = preg_split($splitSongRegex, $streamData, -1, PREG_SPLIT_DELIM_CAPTURE);
+	$artist = $songArray[0];
+	$track = $songArray[1];
+	exit();
+?>
